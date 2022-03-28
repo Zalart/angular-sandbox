ab8ae4342b89ec66205aea6fe1bc2ca20fe9cc10
Zalart <me@zalart.com>
2022-03-28 21:56:53 +0300

    Component files

---
+++ src/app/common/components/searchfield/searchfield.component.ts
@@ -0,0 +1,35 @@
+import {AfterContentChecked, Component, Input, OnChanges, OnInit} from '@angular/core';
+import {SearchService} from "../../services/search.service";
+import {TimeInterval} from "rxjs";
+
+@Component({
+  selector: 'app-searchfield',
+  templateUrl: './searchfield.component.html',
+  styleUrls: ['./searchfield.component.scss'],
+  providers: [SearchService],
+})
+export class SearchfieldComponent implements AfterContentChecked{
+  public queryString = '';
+  public searchResult: {country: string}[] = [];
+  public isActive: boolean = false;
+
+  constructor(private searchService: SearchService ) { }
+ngAfterContentChecked(): void {
+  this.isActive = Boolean(this.queryString && this.searchResult.length > 0);
+  this.searchResult = this.searchService.searchRequest(this.queryString);
+}
+onEnter(event: any): void{
+    setTimeout( ()=> {
+    this.queryString = event.target.value;
+  },1000)
+}
+onSelect(country: string): void {
+    this.queryString = country;
+    this.searchResult = [];
+    this.isActive = !this.isActive;
+}
+onClear(): void {
+    this.queryString = '';
+}
+
+}
