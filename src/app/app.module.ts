decdad82880f963acd78192070f2320eb753741b
Zalart <me@zalart.com>
2022-03-30 12:04:28 +0300

    host listener to bind to click events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { SwitchComponent } from './common/components/switch/switch.component';
 import { SearchFieldComponent } from './common/components/searchfield/searchfield.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { DebounceClickDirective } from './common/directives/debounce-click.directive';
+import { ClickLogComponent } from './click-log/click-log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SwitchComponent,
     SearchFieldComponent,
+    DebounceClickDirective,
+    ClickLogComponent,
   ],
   imports: [
     BrowserModule,
