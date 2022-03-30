0e88a6b125503764f29d55c8767e374da43bb8a0
Zalart <me@zalart.com>
2022-03-30 13:08:13 +0300

    observable subscription adn emitting ligic moved to ngOnInit. Unsubscribe implemented

--- src/app/common/directives/debounce-click.directive.ts
+++ src/app/common/directives/debounce-click.directive.ts
@@ -1,21 +1,26 @@
-import {Directive, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
+import {Directive, EventEmitter, HostListener, OnDestroy, OnInit, Output} from '@angular/core';
 import {debounceTime, Subject} from "rxjs";
 
 @Directive({
   selector: '[appDebounceClick]'
 })
 
-export class DebounceClickDirective implements OnInit {
+export class DebounceClickDirective implements OnInit, OnDestroy {
   @Output() debounceClick = new EventEmitter();
   observable = new Subject();
 
   constructor() {
-    this.observable.pipe(debounceTime(1000)).subscribe((click) => this.debounceClick.emit(click));
+
   }
 
   ngOnInit() {
+    this.observable.pipe(debounceTime(1000)).subscribe((click) => this.debounceClick.emit(click));
   }
-@HostListener('click', ['$event']) onNativeClick (event: Event) {
+  ngOnDestroy() {
+    this.observable.unsubscribe();
+  }
+
+  @HostListener('click', ['$event']) onNativeClick (event: Event) {
     this.observable.next(event);
 }
 }
