e5b574cae6ae0f4a4d002ce1264bc47e0eef0b1e
Zalart <me@zalart.com>
2022-03-31 12:09:59 +0300

    viewContainer clear method before embedding cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import {Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public isUserActive: boolean = false;
   public isSwitchDisabled: boolean = false;
+
+  ngOnInit() {
+  }
 
   onToggleState(): void {
     this.isUserActive = !this.isUserActive;
--- src/app/common/directives/repeat.directive.ts
+++ src/app/common/directives/repeat.directive.ts
@@ -5,7 +5,9 @@ import {Directive, Input, TemplateRef, ViewContainerRef} from "@angular/core";
 })
 export class RepeatDirective {
 constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef ) {}
+
   @Input() set shFor(repeat: number) {
+  this.viewContainer.clear();
   for (let i = 1; i <= repeat; i++) {
     this.viewContainer.createEmbeddedView(
       this.templateRef, {
